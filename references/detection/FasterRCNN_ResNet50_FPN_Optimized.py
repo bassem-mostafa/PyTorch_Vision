a5eeca7fa5886a4861b809da9cd1a9c7d91b56dc
Bassem Mostafa <bassem.ibrahim.ext@orange.com>
2024-09-22 20:05:19 +0300

    use Python convention for private methods, classes, ...

--- references/detection/FasterRCNN_ResNet50_FPN_Optimized.py
+++ references/detection/FasterRCNN_ResNet50_FPN_Optimized.py
@@ -8,9 +8,9 @@ def _rename_module(module, old_name, new_name):
     from collections import OrderedDict
     module.__dict__['_modules'] = OrderedDict([(new_name, v) if k == old_name else (k, v) for k, v in module.__dict__['_modules'].items()])
     
-class DepthWiseSeparable2D(nn.Module):
+class _DepthWiseSeparable2D(nn.Module):
     def __init__(self, inputChannel, outputChannel,stride, kernel_size = 3, padding = 1, bias = False):
-        super(DepthWiseSeparable2D, self).__init__()
+        super(_DepthWiseSeparable2D, self).__init__()
         self.DepthWise = nn.Conv2d(inputChannel,inputChannel, stride=stride, kernel_size = kernel_size, padding = padding, groups = inputChannel, bias = bias)
         self.PointWise = nn.Conv2d(inputChannel, outputChannel, stride=1, kernel_size = 1, bias = bias)
 
@@ -22,14 +22,14 @@ class DepthWiseSeparable2D(nn.Module):
     
 
 #Here we will replace Relu module with Gelu in "FasterRCNN_Optimized" model..............
-def Apply_GELU_(model):
+def _Apply_GELU_(model):
 
     for child_name, child in model.named_children():
         if isinstance(child, nn.ReLU):
             #replace the relu layer with gelu
             setattr(model, child_name, nn.GELU())
         else:
-            Apply_GELU_(child)
+            _Apply_GELU_(child)
 
 
 
@@ -81,32 +81,32 @@ class FasterRCNN_Optimized(FasterRCNN):
         if True:
             # Updating backbone using depth-wise separable convolution instead of standard convolution
             #For Layer_1
-            backbone.body.layer1[0].conv2=DepthWiseSeparable2D(64, 64, kernel_size=(3, 3), stride=(1, 1)) #; _rename_module(backbone.body.layer1[0], "conv2", "DWS2")
-            backbone.body.layer1[1].conv2=DepthWiseSeparable2D(64, 64, kernel_size=(3, 3), stride=(1, 1))
-            backbone.body.layer1[2].conv2=DepthWiseSeparable2D(64, 64, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer1[0].conv2=_DepthWiseSeparable2D(64, 64, kernel_size=(3, 3), stride=(1, 1)) #; _rename_module(backbone.body.layer1[0], "conv2", "DWS2")
+            backbone.body.layer1[1].conv2=_DepthWiseSeparable2D(64, 64, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer1[2].conv2=_DepthWiseSeparable2D(64, 64, kernel_size=(3, 3), stride=(1, 1))
     
             #For Layer_2 
-            backbone.body.layer2[0].conv2=DepthWiseSeparable2D(128, 128, kernel_size=(3, 3), stride=(2, 2))
-            backbone.body.layer2[1].conv2=DepthWiseSeparable2D(128, 128, kernel_size=(3, 3), stride=(1, 1))
-            backbone.body.layer2[2].conv2=DepthWiseSeparable2D(128, 128, kernel_size=(3, 3), stride=(1, 1))
-            backbone.body.layer2[3].conv2=DepthWiseSeparable2D(128, 128, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer2[0].conv2=_DepthWiseSeparable2D(128, 128, kernel_size=(3, 3), stride=(2, 2))
+            backbone.body.layer2[1].conv2=_DepthWiseSeparable2D(128, 128, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer2[2].conv2=_DepthWiseSeparable2D(128, 128, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer2[3].conv2=_DepthWiseSeparable2D(128, 128, kernel_size=(3, 3), stride=(1, 1))
     
             #For Layer_3
-            backbone.body.layer3[0].conv2=DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(2, 2))
-            backbone.body.layer3[1].conv2=DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
-            backbone.body.layer3[2].conv2=DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
-            backbone.body.layer3[3].conv2=DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
-            backbone.body.layer3[4].conv2=DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
-            backbone.body.layer3[5].conv2=DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer3[0].conv2=_DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(2, 2))
+            backbone.body.layer3[1].conv2=_DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer3[2].conv2=_DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer3[3].conv2=_DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer3[4].conv2=_DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer3[5].conv2=_DepthWiseSeparable2D(256, 256, kernel_size=(3, 3), stride=(1, 1))
     
             #For Layer_4
-            backbone.body.layer4[0].conv2=DepthWiseSeparable2D(512, 512, kernel_size=(3, 3), stride=(2, 2))
-            backbone.body.layer4[1].conv2=DepthWiseSeparable2D(512, 512, kernel_size=(3, 3), stride=(1, 1))
-            backbone.body.layer4[2].conv2=DepthWiseSeparable2D(512, 512, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer4[0].conv2=_DepthWiseSeparable2D(512, 512, kernel_size=(3, 3), stride=(2, 2))
+            backbone.body.layer4[1].conv2=_DepthWiseSeparable2D(512, 512, kernel_size=(3, 3), stride=(1, 1))
+            backbone.body.layer4[2].conv2=_DepthWiseSeparable2D(512, 512, kernel_size=(3, 3), stride=(1, 1))
         
         if True:
             # Updating backbone using GELU activation instead of Relu
-            Apply_GELU_(self)
+            _Apply_GELU_(self)
 
         # The following snippets modifies the backbone architecture by different ways.
         
